7913b11353a7f9f3d489c9f0a4944963d2348964
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2022-11-03 16:49:31 +0700

    create method input

--- Exercise2_Algoritma/Program.cs
+++ Exercise2_Algoritma/Program.cs
@@ -14,5 +14,31 @@ namespace LinearTest
         int n;
         //get the number of element to store in the array
         int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("enter the number of element in the array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 74))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and minimum 74 element.\n");
+            }
+            //accept array element
+            Console.WriteLine("");
+            Console.WriteLine("---------------------");
+            Console.WriteLine(" Enter Array Element ");
+            Console.WriteLine("---------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine('<' + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
+
     }
 }
